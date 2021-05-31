454750505cd9ec8106136dfe0aa7da1f3fcfa86a
Rizal Fauzi <rizalfauzi774@gmail.com>
2021-05-31 13:49:29 +0700

    update

--- app/Http/Controllers/AddDoDtlController.php
+++ app/Http/Controllers/AddDoDtlController.php
@@ -31,25 +31,50 @@ class AddDoDtlController extends Controller
                 	return $response;
     			}
 
-    			$data = DB::table('do_dtl')
+    			// $data = DB::table('do_dtl')
+                // ->select(
+                // 	'do_dtl.do_no',
+                // 	'do_dtl.sso_no',
+                //     'do_dtl.descript',
+                //     'do_dtl.unit',
+                //     'do_dtl.quantity',
+                //     'do_dtl.price',
+                //     'do_dtl.cost',
+                //     'do_dtl.itemcode',
+                //     'do_dtl.part_no'
+                // )
+                // ->where(function ( $query) use ($request)
+                // {
+                //     $query->where('do_dtl.do_no', $request->input('do_no'))
+                //             ->orWhere('do_dtl.do_no', $request->input('do_no2'))
+                //             ->orWhere('do_dtl.do_no', $request->input('do_no3'))
+                //             ->orWhere('do_dtl.do_no', $request->input('do_no4'))
+                //             ->orWhere('do_dtl.do_no', $request->input('do_no5'))
+                //             ;
+                // })
+                // ->limit(106)
+                // ->get();
+
+                $data = DB::table('entry_do_tbl')
+                ->leftJoin('item','item.ITEMCODE','=','entry_do_tbl.item_code')
                 ->select(
-                	'do_dtl.do_no',
-                	'do_dtl.sso_no',
-                    'do_dtl.descript',
-                    'do_dtl.unit',
-                    'do_dtl.quantity',
-                    'do_dtl.price',
-                    'do_dtl.cost',
-                    'do_dtl.itemcode',
-                    'do_dtl.part_no'
+                	'entry_do_tbl.do_no',
+                	'entry_do_tbl.sso_no',
+                    'item.DESCRIPT',
+                    'entry_do_tbl.unit',
+                    'entry_do_tbl.quantity',
+                    'item.PRICE',
+                    'item.COST',
+                    'entry_do_tbl.item_code',
+                    'item.PART_NO'
                 )
                 ->where(function ( $query) use ($request)
                 {
-                    $query->where('do_dtl.do_no', $request->input('do_no'))
-                            ->orWhere('do_dtl.do_no', $request->input('do_no2'))
-                            ->orWhere('do_dtl.do_no', $request->input('do_no3'))
-                            ->orWhere('do_dtl.do_no', $request->input('do_no4'))
-                            ->orWhere('do_dtl.do_no', $request->input('do_no5'))
+                    $query->where('entry_do_tbl.do_no', $request->input('do_no'))
+                            ->orWhere('entry_do_tbl.do_no', $request->input('do_no2'))
+                            ->orWhere('entry_do_tbl.do_no', $request->input('do_no3'))
+                            ->orWhere('entry_do_tbl.do_no', $request->input('do_no4'))
+                            ->orWhere('entry_do_tbl.do_no', $request->input('do_no5'))
                             ;
                 })
                 ->limit(106)
